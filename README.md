cd8cda50b80f0740c54c2f69243555dec9c174ed
plaktoz <plaktoz@gmail.com>
2022-01-09 16:06:53 +0800

    Initiate first rest app from spring website
    To start rest api code base from spring website

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # java-app
 java application
+
+To test out the following application
+- batch app (spring base)
+- rest app (spring base)
+- web app (spring base)
---
+++ restapp/src/test/java/com/plaktoz/demo/restapp/RestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plaktoz.demo.restapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
