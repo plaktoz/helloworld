c2a98fc5121feb1a82912bdf81014d115cfeaca0
tianhong <tian.hong.lee@ibm.com>
2025-08-03 16:44:18 +0800

    able to get it run

--- todoist/backend/todoistapp/src/main/java/com/plaktoz/todoist/todoistapp/TodoistappApplication.java
+++ todoist/backend/todoistapp/src/main/java/com/plaktoz/todoist/todoistapp/TodoistappApplication.java
@@ -2,12 +2,20 @@ package com.plaktoz.todoist.todoistapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
 
 @SpringBootApplication
+@RestController
 public class TodoistappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoistappApplication.class, args);
 	}
+
+	@GetMapping("/")
+    public String hello() {
+        return "Hello, World!";
+    }
 
 }
