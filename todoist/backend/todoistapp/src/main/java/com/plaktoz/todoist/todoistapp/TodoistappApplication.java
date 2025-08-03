cc02d616047068ddf4bfc651b3a367fcc234eee1
tianhong <tian.hong.lee@ibm.com>
2025-08-03 16:10:43 +0800

    first commit

---
+++ todoist/backend/todoistapp/src/main/java/com/plaktoz/todoist/todoistapp/TodoistappApplication.java
@@ -0,0 +1,13 @@
+package com.plaktoz.todoist.todoistapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoistappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoistappApplication.class, args);
+	}
+
+}
