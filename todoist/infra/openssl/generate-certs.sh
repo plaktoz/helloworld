932d0cf65e8b934e002859ca49874a4e9b9efa18
tianhong <tian.hong.lee@ibm.com>
2025-08-24 18:23:39 +0800

    make openssl generate cert work without asking for commit

---
+++ todoist/infra/openssl/generate-certs.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+apk add --no-cache openssl
+mkdir -p /certs
+
+
+## Generate Root CA
+openssl genrsa -out /certs/ca.key 2048
+openssl req -new -x509 -days 3650 -key /certs/ca.key -out /certs/ca.crt \
+  -subj '/C=US/ST=State/L=City/O=Organization/CN=Root CA'
+
+# Generate Server Certificate
+openssl genrsa -out /certs/server.key 2048
+openssl req -new -key /certs/server.key -out /certs/server.csr \
+  -subj '/C=US/ST=State/L=City/O=Organization/CN=localhost'
+openssl x509 -req -days 3650 -in /certs/server.csr -CA /certs/ca.crt \
+  -CAkey /certs/ca.key -CAcreateserial -out /certs/server.crt
+
+## Generate Client Certificate
+#openssl genrsa -out /certs/client.key 2048
+#openssl req -new -key /certs/client.key -out /certs/client.csr \
+#  -subj '/C=US/ST=State/L=City/O=Organization/CN=client'
+#openssl x509 -req -days 3650 -in /certs/client.csr -CA /certs/ca.crt \
+#  -CAkey /certs/ca.key -CAcreateserial -out /certs/client.crt
+
+# Clean up CSR files
+rm /certs/*.csr /certs/*.srl
+
+echo 'Certificates generated successfully!'
+ls -la /certs/
